e0909a77951cb4cc1b6d8ae69fd7a31070ca5410
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-15 22:37:47 +0800

    Update app.py

--- stream/app.py
+++ stream/app.py
@@ -5,18 +5,17 @@ KAFKA_IP = os.getenv('KAFKA_IP', 'localhost')
 KAFKA_PORT = os.getenv('KAFKA_PORT', '9092')
 KAFKA_TOPIC = os.getenv('KAFKA_TOPIC', 'car_data')
 
-kafka_utils.create_topic(KAFKA_IP, KAFKA_PORT, KAFKA_TOPIC)
 
-# topic_list = []
-# topic_list.append(NewTopic(KAFKA_TOPIC, 1, 1))
-# admin_client.create_topics(topic_list)
-
+topic_list = []
+topic_list.append(NewTopic(KAFKA_TOPIC, 1, 1))
+admin_client.create_topics(topic_list)
 
 
 
-# admin_client = AdminClient({
-#     "bootstrap.servers": KAFKA_IP+':'+KAFKA_PORT
-# })
+
+admin_client = AdminClient({
+    "bootstrap.servers": KAFKA_IP+':'+KAFKA_PORT
+})
 
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_IP+':'+KAFKA_PORT)
