4af02ab3584d736a17d979d0fc1c0a596beacaaf
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-16 01:20:12 +0800

    Update app.py

--- stream/app.py
+++ stream/app.py
@@ -3,6 +3,15 @@ from kafka import KafkaProducer
 import os
 
 from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka import Producer
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
 KAFKA_IP = os.getenv('KAFKA_IP')
 KAFKA_PORT = os.getenv('KAFKA_PORT')
@@ -19,6 +28,7 @@ topic_list.append(NewTopic(KAFKA_TOPIC, 1, 1))
 admin_client.create_topics(topic_list)
 
 print(KAFKA_IP+':'+KAFKA_PORT)
-producer = KafkaProducer(bootstrap_servers=KAFKA_IP+':'+KAFKA_PORT)
-producer.send(KAFKA_TOPIC, b'Hello, World!')
-producer.send(KAFKA_TOPIC, key=b'message-two', value=b'This is Kafka-Python')
+
+p = Producer({'bootstrap.servers': KAFKA_IP+':'+KAFKA_PORT})
+p.produce('mytopic', "test", callback=delivery_report)
+p.flush()
