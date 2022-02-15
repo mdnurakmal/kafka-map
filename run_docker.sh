465fc2696c7cef27d1114901422f2ab8d9606722
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-15 22:42:38 +0800

    Create run_docker.sh

---
+++ run_docker.sh
@@ -0,0 +1,9 @@
+echo Killing old Docker processes
+docker-compose rm -fs
+
+echo Building Docker images
+docker-compose build
+
+echo Starting Docker containers
+docker-compose up -d kowl
+docker-compose up -d stream
