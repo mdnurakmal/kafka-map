d91bdedcdcc6e7fc07c08cc86e4c2afc3a938ae7
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-16 13:18:13 +0800

    update

--- frontend/static/leaf.js
+++ frontend/static/leaf.js
@@ -2,7 +2,8 @@ var mymap = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
+    id: 'mapbox/streets-v11',
+    zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiaW5leGlzdGVkIiwiYSI6ImNrenAwZnZiMDNlMHcyb29jeHo5MmE1MXcifQ.TNEIV1_enrRJCpZkBJcIrA' //ENTER YOUR ACCESS TOKEN HERE
 }).addTo(mymap);
 
