1ff4a5f2e73e7fc117ad0451fe5da00d3efedb61
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-16 13:54:23 +0800

    Update leaf.js

--- frontend/static/leaf.js
+++ frontend/static/leaf.js
@@ -3,7 +3,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
-    zoomOffset: -1,
+
     accessToken: 'pk.eyJ1IjoiaW5leGlzdGVkIiwiYSI6ImNrenAwZnZiMDNlMHcyb29jeHo5MmE1MXcifQ.TNEIV1_enrRJCpZkBJcIrA' //ENTER YOUR ACCESS TOKEN HERE
 }).addTo(mymap);
 
