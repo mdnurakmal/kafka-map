041a35775725baed5d4da72b0d66079b8d74cdc7
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-16 11:44:51 +0800

    update

--- frontend/static/leaf.js
+++ frontend/static/leaf.js
@@ -1,16 +1,16 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: 'ABCDEFG' //ENTER YOUR ACCESS TOKEN HERE
+    accessToken: 'pk.eyJ1IjoiaW5leGlzdGVkIiwiYSI6ImNrenAwZnZiMDNlMHcyb29jeHo5MmE1MXcifQ.TNEIV1_enrRJCpZkBJcIrA' //ENTER YOUR ACCESS TOKEN HERE
 }).addTo(mymap);
 
 mapMarkers1 = [];
 mapMarkers2 = [];
 mapMarkers3 = [];
 
-var source = new EventSource('/topic/TOPICNAME'); //ENTER YOUR TOPICNAME HERE
+var source = new EventSource('/topic/myyopic'); //ENTER YOUR TOPICNAME HERE
 source.addEventListener('message', function(e){
 
   console.log('Message');
