1fe8c05fecde4bca389c3a9d7b5961859683cef0
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-17 14:01:28 +0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -9,7 +9,7 @@ docker-compose build
 echo Starting Docker containers
 docker-compose up -d kowl
 docker-compose up -d stream
-docker-compose up -d stream2
+#docker-compose up -d stream2
 docker-compose up -d frontend
 docker-compose up -d temp
 
